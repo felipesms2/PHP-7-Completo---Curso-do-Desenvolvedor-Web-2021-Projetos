aabffbfa9bf5ff0fd5a5e633eb83e34d29c98409
Felipe <felipesms@live.com>
2021-09-26 17:06:54 -0200

    fixing tag bugs on exercicio

--- exercicio.php
+++ exercicio.php
@@ -6,13 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Curso PHP</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/exercicio.css">
 
 </head>
-<body>
+<body class="exercicio">
     <header class="cabecalho">
         <h1>Curso PHP</h1>
         <h2>Visualização do Exercício</h2>
+
     </header>
+
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem formatação</a>
+        <a href="#" class="vermelho">Voltar</a>
+         </nav>
+
     <main class="principal">
         <div class="conteudo">
             
