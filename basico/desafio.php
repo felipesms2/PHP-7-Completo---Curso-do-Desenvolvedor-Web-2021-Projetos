a77ae2c77a7da08c38da4c4c7614d38940bacd77
Felipe <felipesms@live.com>
2021-09-29 05:49:03 -0200

    created file chalenge to complete chapter

---
+++ basico/desafio.php
@@ -0,0 +1,12 @@
+<div class="titulo">
+    <ul>
+        <li>1+1= <?= 1+1?></li>
+        <li>1+1= <?= 4+4?></li>
+        <?php
+            $item1 = 8;
+            $item2 = 8;
+            $sum = $item1 + $item2;
+        ?>
+        <li><?php echo"$item1 + $item2= " . $sum ;?></li>
+    </ul>
+</div>
