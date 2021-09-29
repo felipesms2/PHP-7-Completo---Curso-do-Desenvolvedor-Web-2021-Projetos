8fbb8d55e3dccb2a9e574b100ed682d10abd4dbe
Felipe <felipesms@live.com>
2021-09-29 01:36:26 -0200

    adding styles inline in included file

--- basico/css.php
+++ basico/css.php
@@ -11,3 +11,16 @@ echo "Olá
 ";
 ?>
 </h1>
+
+<button>Teste</button>
+
+<style>
+    button
+        {
+            padding: 5px 20px;
+            background-color: #4286f4;
+            color: #EEE;
+            font-weight: bold;
+            border-radius: 10px;
+        }
+</style>
