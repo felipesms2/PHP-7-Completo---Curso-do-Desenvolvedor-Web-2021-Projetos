1a872c52b0a9e9b21be26f20355f0200896d543b
Felipe <felipesms@live.com>
2021-09-30 16:38:11 -0200

    Linking another module on course

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <div class="conteudo">
             <nav class="modulos">
                 <div class="modulo verde">
-                    <h3>Módulo 01 - Básico</h3>
+                    <h3>Básico</h3>
                     <ul>
                         <li>
                             <a href="exercicio.php?dir=basico&file=ola">
@@ -44,6 +44,17 @@
                                 Desafio
                             </a>
                         </li>                        
+                    </ul>
+                </div>
+
+                <div class="modulo vermelho">
+                    <h3>Tipos</h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=ola">
+                                Tipo Inteiro
+                            </a>
+                        </li>                      
                     </ul>
                 </div>
                             
