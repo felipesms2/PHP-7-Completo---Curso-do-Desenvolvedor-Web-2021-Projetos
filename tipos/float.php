17e4170529dff14216aba5c7e1eac3c81beff0d6
Felipe <felipesms@live.com>
2021-10-01 17:03:34 -0200

    Making float operations and dumping the vars

---
+++ tipos/float.php
@@ -0,0 +1,15 @@
+<?php
+
+    echo 1.1;
+    echo "<br>";
+    var_dump(1.1);
+
+    echo "<br>
+    Float MAX: " . PHP_FLOAT_MAX .  "
+    <br>
+    Float MIN: " . PHP_FLOAT_MIN . 
+    
+    "<br>" . 1.2e3 . 
+    "<br>" . 13E-3
+    ;
+?>
