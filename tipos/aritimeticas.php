098f76101dfb43f0d383b60ef9a56990f9bb70cb
Felipe <felipesms@live.com>
2021-10-01 19:13:28 -0200

    Finishing aritmethics rules and header

---
+++ tipos/aritimeticas.php
@@ -0,0 +1,17 @@
+<div class="titulo">
+    Operações Aritiméticas
+</div>
+
+<?php
+    echo 1 + 1 . "<br>";
+    echo 1 + 2.5 . "<br>";
+    echo 10-2 . "<br>";
+    echo 2*5 . "<br>";
+    echo 7/4 . "<br>";
+    echo intdiv(7,4) . "<br>";
+    echo round(7/4) . "<br>";
+    echo 7%4 . "<br>";
+    echo 7%2 . "<br>";
+    echo 8%2 . "<br>";
+    ;
+?>
