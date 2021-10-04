61429deb5227d099102710e9e7a7c1b0c75de9bc
Felipe <felipesms@live.com>
2021-10-04 03:41:36 -0200

    Linking boolean type

--- index.php
+++ index.php
@@ -80,6 +80,11 @@
 				Tipo String        	
                             </a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=booleano">
+				Tipo Booleano        	
+                            </a>
+                        </li>
                     </ul>
                 </div>
                             
