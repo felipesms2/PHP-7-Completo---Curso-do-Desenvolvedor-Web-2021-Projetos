d064a9f40543c46bb38e75bd10c70f7ec0a5edf2
Felipe <felipesms@live.com>
2021-10-06 18:23:01 -0300

    Finish topic 52 starting basic variables

--- variaveis/basico.php
+++ variaveis/basico.php
@@ -1,1 +1,26 @@
 <div class="titulo">Variaveis</div>
+
+<?php
+    $numeroA = 13;
+    echo $numeroA, "<br>";
+    var_dump($numeroA);
+
+    echo "<br>";
+
+    $a = 13;
+    $b = 16;
+    $soma = $a + $b;
+    echo $soma;
+    echo "<br>";
+    $somadosnumeros =0;
+    echo isset($somadosnumeros);
+    echo "<br>";
+    unset($somadosnumeros);
+    var_dump($somadosnumeros);
+    $variavel = 10;
+    echo "<br> $variavel";
+    $variavel = "Agora sou uma string";
+    echo "<br> $variavel";
+
+
+?>
