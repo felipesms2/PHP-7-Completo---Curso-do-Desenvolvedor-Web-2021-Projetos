8ab51f32ddcbf8877912eb0407792402a1d090cd
Felipe <felipesms@live.com>
2021-10-12 12:58:11 -0300

    Finishing interpolation

---
+++ variaveis/interpolacao.php
@@ -0,0 +1,12 @@
+<div class="titulo">Interpolações</div>
+
+<?php
+
+$numero = 10;
+echo "<br> $numero";
+echo "<br> sua nota é $numero";
+$objeto = 'caneta';
+echo "<br> eu tenho 5 {$objeto}s";
+echo "<br> eu tenho uma $objeto";
+
+?>
