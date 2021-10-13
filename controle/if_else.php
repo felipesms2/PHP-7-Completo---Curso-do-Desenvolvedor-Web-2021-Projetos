984bbb60ed86a8652a9ef179f0d1370df149520c
Felipe <felipesms@live.com>
2021-10-13 14:55:51 -0300

    Finishing section 69 part 2 if-else

--- controle/if_else.php
+++ controle/if_else.php
@@ -1,10 +1,40 @@
 <div class="titulo">IF Else</div>
 
     <?php
-        if(true)
+            if(true) 
             {
-                echo "Serei impresso?";
-                echo "Serei impresso novamente?";
+                echo "Serei impresso?<br>";
             }
-      
+
+            if(false) 
+            {
+                echo "Verdadeiro - Parte A<br>";
+                echo "Verdadeiro - Parte B<br>";
+            } else 
+            {
+                echo "Falso - Parte A<br>";
+                echo "Falso - Parte B<br>";
+            }
+
+            if(false) 
+            {
+                echo "Passo A<br>";
+            } else if(false) 
+            {
+                echo "Passo B<br>";
+            } elseif(false) 
+            {
+                echo "Passo C<br>";
+            } elseif(false) 
+            {
+                echo "Passo D<br>";
+            } elseif(false) 
+            {
+                echo "Passo E<br>";
+            } else 
+            {
+                echo "Úlitmo Passo<br>";
+            }
+
+            echo "Fim<br>";
     ?>
