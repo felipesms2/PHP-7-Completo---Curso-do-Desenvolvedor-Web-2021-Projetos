24780d0c6ef225de42dd0368f82f0fad33276b2c
Felipe <felipesms@live.com>
2021-10-13 15:17:47 -0300

    Finishing relational operators pt2  section 71

--- controle/operadores_relacionais.php
+++ controle/operadores_relacionais.php
@@ -18,4 +18,17 @@
         echo "Terceira idade = $idade anos!";
     }
 
+    echo '<p>Spaceship</p><hr>';
+    var_dump(500 <=> 3);
+    var_dump(50 <=> 50);
+    var_dump(5 <=> 50);
+
+
+    echo '<p>Valores pode ser V ou F</p><hr>';
+    var_dump(!!5);
+    var_dump(!!0);
+    var_dump(!!"");
+    var_dump(!!" ");
+
+
 ?>
