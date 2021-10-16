0ed1d0e5b39347667af3a8060d5783d5b70668e1
Felipe <felipesms@live.com>
2021-10-16 17:19:47 -0300

    Section 80 starting switch

---
+++ controle/switch.php
@@ -0,0 +1,9 @@
+    <div class="titulo">
+        Operadores Ternários
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+        ?> 
