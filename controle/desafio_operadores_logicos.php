7e4d1d83437622f46a38f16519ed2e894dad4531
Felipe <felipesms@live.com>
2021-10-16 13:18:53 -0300

    Starting section 77 challenge logical operator

---
+++ controle/desafio_operadores_logicos.php
@@ -0,0 +1,17 @@
+<div class="titulo">
+    Operadores Lógicos
+</div>
+
+<style>
+
+.boldP
+    {
+        font-weight: bold;
+    }
+
+</style>
+<?php
+    include("customClass.php");
+    $display = new excercise();  
+
+?>
