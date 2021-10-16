65f55be5564bc90fe8ae3e85ab700372dbe5e6d7
Felipe <felipesms@live.com>
2021-10-16 20:29:53 -0300

    Section 80 finishing switch

--- controle/switch.php
+++ controle/switch.php
@@ -1,9 +1,33 @@
     <div class="titulo">
-        Operadores Ternários
+        Switch Case
     </div>
         <?php
             include("customClass.php");
             $display = new excercise();
-            //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
+            $categoria;
+            //var_dump($categoria);
+            //die();
+            $categoria =  "popular";
+
+            switch ($categoria) 
+            {
+                case 'Luxo':
+                    $preco = "100k+";
+                    break;
+                case "SUV":
+                    $preco = "50k+";
+                    break;
+                case 'popular':
+                    $preco = "30k+";
+                    break;
+                
+                default:
+                    $preco;    
+                break;
+            }
+            $display->displayAndBroke("Categoria $categoria");
+            $display->displayAndBroke("Preço $preco");
+           
+            
         ?> 
