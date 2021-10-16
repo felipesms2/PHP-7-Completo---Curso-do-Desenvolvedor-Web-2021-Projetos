b914002823037809b97f8ff0e0997009df35c419
Felipe <felipesms@live.com>
2021-10-16 17:17:13 -0300

    Section 79 finishing Ternary Operator

--- controle/operador_ternario.php
+++ controle/operador_ternario.php
@@ -2,5 +2,22 @@
         Operadores Ternários
     </div>
         <?php
-            
+            include("customClass.php");
+            $display = new excercise();
+
+            $idade =70;
+            $status;
+            if($idade >18)
+                {
+                    $status = "Maior de idade";
+                }
+            else
+                {
+                    $status = "Menor de idade";
+                }
+            $display->displayAndBroke($status);
+        
+            $idade = 17;
+            $status = $idade>=18 ? "Maior de idade" : "Menor de idade";
+            $display->displayAndBroke($status);
         ?> 
