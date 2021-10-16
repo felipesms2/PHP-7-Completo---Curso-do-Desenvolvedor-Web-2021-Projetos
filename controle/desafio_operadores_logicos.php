6aced8b6433597f2ce6a13e5fa97e8731c762216
Felipe <felipesms@live.com>
2021-10-16 14:23:45 -0300

    Section 77 - finishing logical operators challenge

--- controle/desafio_operadores_logicos.php
+++ controle/desafio_operadores_logicos.php
@@ -3,7 +3,76 @@
     </div>
 
 
+
+    <form action="#" method="post">
+    <div>
+        <label for="t1">Trabalho 1 (Terça):</label>
+        <select name="t1" id="t1">
+            <option value="1">Executado</option>
+            <option value="0">Não Executado</option>
+        </select>
+    </div>
+    <div>
+        <label for="t2">Trabalho 2 (Quinta):</label>
+        <select name="t2" id="t2">
+            <option value="1">Executado</option>
+            <option value="0">Não Executado</option>
+        </select>
+    </div>
+    <button>Executar</button>
+</form>
+
+
+
     <?php
         include("customClass.php");
-        $display = new excercise();  
-    ?> 
+        $display = new excercise();
+        
+
+
+        if(isset($_POST['t1']) && isset($_POST['t2'])) 
+        {
+            $t1 = $_POST['t1'] === '1';
+            $t2 = !!$_POST['t2'];
+            $tv = '';
+            $sorvete = false;
+
+            if($t1 && $t2) 
+            {
+                $tv = '50"';
+            } elseif($t1 xor $t2) 
+            {
+                $tv = '32"';
+            }
+
+            if($t1 or $t2) 
+            {
+                $sorvete = true;
+            }
+
+            if($tv) 
+            {
+                $resultado = "Vamos comprar uma TV de $tv";
+            } 
+            else 
+            {
+                $resultado = "Sem TV dessa vez :(";
+            }
+
+            $saudavel = !$sorvete;
+
+            if($saudavel) 
+            {
+                $resultado .= '<br>Estamos mais saudáveis!';
+            } 
+            else 
+            {
+                $resultado .= '<br>Sorvete liberado \o/';
+            }
+
+            echo "<p>$resultado</p>";
+        }
+
+
+        
+        ?> 
