64f7e41ef17267a243c77e3274d846c12a0d9525
Felipe <felipesms@live.com>
2021-10-25 02:46:08 -0300

    Commiting file by webBrowser

--- controle/desafio_switch.php
+++ controle/desafio_switch.php
@@ -5,6 +5,10 @@
 
             include("customClass.php");            
             $display = new excercise();
+            //$scriptFileName = "." . $_SERVER["SCRIPT_FILENAME"];
+            //echo $scriptFileName;
+
+
             //echo "<pre>", var_dump($_SERVER);
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
