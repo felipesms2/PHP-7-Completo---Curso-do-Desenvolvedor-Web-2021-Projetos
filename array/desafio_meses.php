fcaf4bc337fd316f4272c8c71beec89f7af7d5e7
Felipe <felipesms@live.com>
2021-10-25 17:52:25 -0300

    Section 92 finish month challenge, we have to add 1 index to keeping foward

--- array/desafio_meses.php
+++ array/desafio_meses.php
@@ -4,5 +4,20 @@
         <?php
             include("customClass.php");
             $display = new excercise();
- 
+            $months = array(1 => "Janeiro",
+                "Fevereiro",
+                "Março",
+                "Abril",
+                "Maio",
+                "Junho",
+                "Julho",
+                "Agosto",
+                "Setembro",
+                "Outubro",
+                "Novembro",
+                "Dezembro"
+                );
+
+            //$display->displayAndBroke($status);
+            $display->dupmAndPre($months);
         ?> 
