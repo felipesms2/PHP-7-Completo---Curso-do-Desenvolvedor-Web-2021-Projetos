cf696f957a8821a913356a93e60f8f020eeb4564
Felipe <felipesms@live.com>
2021-10-25 16:58:54 -0300

    Starting section 91 challenge index

---
+++ array/desafio_index.php
@@ -0,0 +1,9 @@
+<div class="titulo">
+        Desafio Index
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+        ?> 
