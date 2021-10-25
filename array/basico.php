f05a18a7ee281e7db9bd4b611352ed0ae00d3c23
Felipe <felipesms@live.com>
2021-10-25 15:52:36 -0300

    Middle course

--- array/basico.php
+++ array/basico.php
@@ -1,5 +1,28 @@
 <div class="titulo">Array Básico</div>
 
 <?php
-   $list = array(1,3,5,'Random text');
+
+      include("customClass.php");
+      $display = new excercise();
+      //$display->displayAndBroke($status);
+      //$display->dumpAndBroke($status);
+      echo "<p>";
+      $list = array(1,3,5,'Random text');
+      
+      $display->displayAndBroke($list[0]);
+      $display->displayAndBroke($list[1]);
+      $display->displayAndBroke($list[2]);
+      $display->displayAndBroke($list[3]);
+      $display->dumpAndBroke($list[3]);
+
+      $texto = "Aqui está um texto de teste com acentuação";
+      $textoUTF8 = utf8_decode($texto);
+
+      $display->displayAndBroke(utf8_encode($textoUTF8));
+      $display->dumpAndBroke($texto);
+
+      $display->displayAndBroke($texto[10]);
+
+      echo "</p>";
+
 ?>
