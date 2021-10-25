21852247ecb91b6f750c2465832d6c66522f395b
Felipe <felipesms@live.com>
2021-10-25 02:47:35 -0300

    Commiting by Browser

--- controle/desafio_switch.php
+++ controle/desafio_switch.php
@@ -8,7 +8,6 @@
             //$scriptFileName = "." . $_SERVER["SCRIPT_FILENAME"];
             //echo $scriptFileName;
 
-
             //echo "<pre>", var_dump($_SERVER);
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
