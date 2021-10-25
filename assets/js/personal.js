7de4ffd3856c7c0f356b2479b1ecb8ff714cf7a8
Felipe <felipesms@live.com>
2021-10-25 00:17:18 -0300

    Personalized js outside course instructions

---
+++ assets/js/personal.js
@@ -0,0 +1,43 @@
+function clipBoard() 
+{
+
+    commitMsg = $("#commitMsg").val();
+    fileName = $("#fileName").val();
+    
+    if(commitMsg=="")
+      {
+          alert("Descrição do commit é obrigatória");
+          return false;
+      }
+    else
+      {
+        gitCommand = "git commit -m '" + commitMsg + "' " + fileName ;
+        var dummy = document.createElement("input");
+        //dummy.style.display = 'none';
+        document.body.appendChild(dummy);
+    
+        dummy.setAttribute("id", "dummy_id");
+        document.getElementById("dummy_id").value=gitCommand;
+        dummy.select();
+        document.execCommand("copy");
+        document.body.removeChild(dummy);
+        $("#popuContent").load("./ajaxGit.php?cmd=" +  encodeURI(gitCommand));
+        $(".popup, .popup-content").addClass("active");
+
+        alert(gitCommand);
+      }
+
+    //alert(id);
+    // valor = $("#" + id).val();
+    // input = document.getElementById(id);
+    // input.focus();
+    // input.select();
+    // document.execCommand("copy");
+}
+
+
+function closePopUp()
+  {
+    $("#popuContent").html("");
+    $(".popup, .popup-content").removeClass("active"); 
+  }
