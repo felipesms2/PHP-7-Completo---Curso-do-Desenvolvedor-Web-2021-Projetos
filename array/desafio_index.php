fac9b3ad8264fd3d42e68183a2a62d1c45e66960
Felipe <felipesms@live.com>
2021-10-25 17:08:56 -0300

    Section 91 finishing challenge index

--- array/desafio_index.php
+++ array/desafio_index.php
@@ -6,4 +6,18 @@
             $display = new excercise();
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
+
+            $listData = array(
+                1,
+                4 =>  2,
+                3 =>  3,
+                'a' =>  4,
+                5,
+                '9' =>  6,
+                '06' => 7,
+                0 => 8
+            );
+        
+            $display->dumpAndBroke($listData);
+
         ?> 
