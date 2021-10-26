9b5e1029fe13ac7a560db1aceedeace00817084e
Felipe <felipesms@live.com>
2021-10-25 23:15:44 -0300

    Section 92 - Starting operations with array

---
+++ array/operacoes.php
@@ -0,0 +1,9 @@
+<div class="titulo">
+        Operações com array
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+        ?> 
