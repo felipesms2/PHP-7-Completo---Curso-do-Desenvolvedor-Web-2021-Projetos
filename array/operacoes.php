35e6df3bb89d389dab2f9ed9c10caeb0e3dabc69
Felipe <felipesms@live.com>
2021-10-25 23:37:07 -0300

    Section 93 - finishing array operations

--- array/operacoes.php
+++ array/operacoes.php
@@ -6,4 +6,41 @@
             $display = new excercise();
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
+
+            $data1 = 
+                [
+                    "name"=>"José",
+                    "age"=>"23",
+                ];
+            $data2 = 
+                [
+                    "bornPlace"=>"Fortaleza",
+                    //"Naturalidade"=>"Fortaleza",
+                ];
+            $data3 = 
+                [
+
+                ];
+
+            $completeData = $data1 + $data2;
+            if(is_array($completeData))
+                {
+                    $display->dumpAndBroke($completeData);
+                }
+            
+            $index = array_rand($completeData);
+            $display->displayAndBroke("$index =  $completeData[$index]");
+
+            unset($completeData);
+            //var_dump($completeData);
+
+            $odd = [1,3,5,7,9];
+            $even = [2,4,6,8];
+            $decimal = $odd + $even;
+            $display->dumpAndBroke($decimal);
+            
+            $decimal = array_merge($odd, $even);
+
+            $display->dumpAndBroke($decimal);
+
         ?> 
