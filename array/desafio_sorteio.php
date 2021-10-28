3c0cd1ec780d9c2bcdb32819622e0f772593f025
Felipe <felipesms@live.com>
2021-10-28 16:10:50 -0300

    Section 94 finishing challenge sorting

--- array/desafio_sorteio.php
+++ array/desafio_sorteio.php
@@ -1,5 +1,14 @@
-<?php
+<div class="titulo">
+        Desafio Sorteio
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
 
+            $randomNames = array("rand1", "rand2", "rand3");
+            $index = array_rand($randomNames);
+            echo $randomNames[$index];
 
-
-?>
+        ?> 
