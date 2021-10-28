20609fdd6214a264e4aa64163c9ba6c27de63da0
Felipe <felipesms@live.com>
2021-10-28 16:36:39 -0300

    Section 95 - starting multidimensional arrays

---
+++ array/multidimensionais.php
@@ -0,0 +1,9 @@
+<div class="titulo">
+        Array multidimensional
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+        ?> 
