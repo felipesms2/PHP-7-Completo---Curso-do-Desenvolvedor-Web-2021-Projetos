3d62ffe49df499906861c19492374caf4b45e602
Felipe <felipesms@live.com>
2021-10-29 14:39:00 -0300

    Section 103 - Starting loop for

---
+++ loops/for.php
@@ -0,0 +1,9 @@
+    <div class="titulo">
+        Loop For
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+        ?> 
