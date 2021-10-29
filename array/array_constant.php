72d22891a1493a48352a8c3d7fbc5688247ced6a
Felipe <felipesms@live.com>
2021-10-29 03:11:34 -0300

    Section 96 - Starting array constant

---
+++ array/array_constant.php
@@ -0,0 +1,10 @@
+<div class="titulo">
+        Array Constant
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+            const FRUTS = ['RaspBerry', 'Carrot'];
+        ?> 
