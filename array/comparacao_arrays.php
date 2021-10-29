cb869504087c15e098db920108a264da9bb3d4f7
Felipe <felipesms@live.com>
2021-10-29 04:37:22 -0300

    Section 99 - Done array comarison

--- array/comparacao_arrays.php
+++ array/comparacao_arrays.php
@@ -6,4 +6,14 @@
             $display = new excercise();
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
+            
+            $arrayName = array('name' => 'felipe', 'age'=>12 );
+            $arrayName2 = array('name' => 'felipe', 'age'=>12 );
+            
+          echo "Non Strict " . $display->dumpAndBroke($arrayName == $arrayName2);
+          echo 'Strict ' . $display->dumpAndBroke($arrayName == $arrayName2);
+          
+          $arrayName3 = array('age'=>12, 'name' => 'felipe');
+          
+          $display->dumpAndBroke($arrayName3===$arrayName2);
         ?> 
