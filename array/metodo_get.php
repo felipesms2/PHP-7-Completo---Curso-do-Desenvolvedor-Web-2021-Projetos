98976f5f2b50910bf2d6ab77a1b14cd3c0f53da9
Felipe <felipesms@live.com>
2021-10-29 03:16:44 -0300

    Section 97 - Starting method get

---
+++ array/metodo_get.php
@@ -0,0 +1,9 @@
+<div class="titulo">
+        Método Get
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+        ?> 
