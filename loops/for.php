8bb5cd68a898cc8b1903a8a192efc687be80a4b2
Felipe <felipesms@live.com>
2021-11-12 23:20:02 -0300

    Section 103 - Finishing for loop

--- loops/for.php
+++ loops/for.php
@@ -6,4 +6,27 @@
             $display = new excercise();
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
+            
+            for($count = 1; $count <= 5; $count++) 
+            {
+                $display->displayAndBroke($count);
+            }
+
+            $display->displayAndBroke("");
+
+            $count =0;
+            for($count>0 ; $count <= 15; ) 
+            {
+                $display->dumpAndBroke($count);
+                $count++;
+            }
+
+            $count = 0;
+            
+            while ($count < 10)
+                {   
+                    $display->displayAndBroke($count);
+                    $count++;
+                }
+
         ?> 
