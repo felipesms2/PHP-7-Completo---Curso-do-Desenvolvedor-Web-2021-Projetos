d5633fca1d0015226b77dd37afaa64aa2b97d5bf
Felipe <felipesms@live.com>
2021-11-18 17:03:49 -0300

    Section 108 - Finishing impression challenge

--- loops/print-challenge.php
+++ loops/print-challenge.php
@@ -6,4 +6,28 @@
             $display = new excercise();
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
+
+            $listChars = [
+                "AAA",
+                "BBB",
+                "CCC",
+                "DDD",
+                "EEE",
+                "FFF"
+            ];
+            
+            for($i = 0; $i < count($listChars); $i++) 
+            {
+                if($i % 2 === 1) continue;
+                 $display->displayAndBroke($listChars[$i]);
+            }
+            
+            echo "<br>";
+            
+            foreach($listChars as $index => $value) 
+            {
+                if($index % 2 !== 0) 
+                continue;
+                $display->displayAndBroke($value);
+            }
         ?> 
