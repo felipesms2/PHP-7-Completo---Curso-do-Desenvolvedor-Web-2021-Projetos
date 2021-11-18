268470b54717ce3092274a24366e6f66b1bd9a7b
Felipe <felipesms@live.com>
2021-11-18 03:26:18 -0300

    Section 107 - Finishing break e continue

--- loops/break-continue.php
+++ loops/break-continue.php
@@ -6,4 +6,34 @@
             $display = new excercise();
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
+            
+            $counter = 10;
+            for (;;) 
+            {
+              $counter++;
+              $display->displayAndBroke($counter);
+              
+              if ($counter >20) 
+              {
+                break;// code...
+              }
+            }
+            $counter = 1;
+            for (;;) 
+            {
+              $counter++;
+              if ($counter % 2 ===1) 
+              {
+                 $display->displayAndBroke($counter);
+              }
+              
+              
+              if ($counter >40) 
+              {
+                break;// code...
+              }
+            }
+            
+            
+            
         ?> 
