3dbe5e804b802cf07935e878dbce296305b5a6d5
Felipe <felipesms@live.com>
2021-11-18 17:28:46 -0300

    Section 110 - Finishing while and do while

--- loops/while.php
+++ loops/while.php
@@ -6,4 +6,25 @@
             $display = new excercise();
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
+
+
+        $counterLimit = 10;
+        $counterStart = 0;
+
+
+        while ($counterStart <= $counterLimit) 
+            {
+                $display->displayAndBroke($counterStart);
+                $counterStart++;
+            }
+
+        echo "<hr>";
+
+            do
+                {
+                    $display->displayAndBroke($counterStart);
+                }
+                while ($counterStart <=$counterLimit);
+
         ?> 
+
