386106e7cd613ad01637232b72b38469d6f56cd3
Felipe <felipesms@live.com>
2021-11-18 16:41:28 -0300

    Section 108 - Starting impression challenge

---
+++ loops/print-challenge.php
@@ -0,0 +1,9 @@
+<div class="titulo">
+        Desafio Impressão
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+        ?> 
