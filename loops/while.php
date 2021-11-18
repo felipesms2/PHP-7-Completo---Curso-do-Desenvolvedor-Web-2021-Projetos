a0546c6def69f15ea54493d329e971a8a05df695
Felipe <felipesms@live.com>
2021-11-18 17:09:36 -0300

    Section 110  - Starting while and do while

---
+++ loops/while.php
@@ -0,0 +1,9 @@
+<div class="titulo">
+        While e do While
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+        ?> 
