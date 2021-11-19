c49ede1b0e44948018c7680946af7f9ee0454707
Felipe <felipesms@live.com>
2021-11-19 02:02:54 -0300

    Section 119 - Finishing functions scope

--- functions/getting-start.php
+++ functions/getting-start.php
@@ -6,4 +6,27 @@
             $display = new excercise();
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
+
+        $toChange = 1;
+
+         function ToChange($display)
+            {
+                $toChange = 2;
+                $display->dumpAndBroke("Current value is $toChange");
+            }
+
+            $display->displayAndBroke("Previous value is $toChange");
+            ToChange($display);
+
+           $display->displayAndBroke("Actualli toChange is $toChange");
+    
+            function staticChange()
+                {
+                    global $toChange;
+                    $toChange = 4;
+                }
+
+            staticChange();
+            $display->displayAndBroke("toChange is changed to $toChange");
+
         ?> 
