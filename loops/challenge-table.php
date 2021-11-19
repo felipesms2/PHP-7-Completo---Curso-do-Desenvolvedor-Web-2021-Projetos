c06686533831aba05000298f740ffba91076a724
Felipe <felipesms@live.com>
2021-11-18 21:42:46 -0300

    Section 111 - Starting Table challenge

---
+++ loops/challenge-table.php
@@ -0,0 +1,9 @@
+<div class="titulo">
+        Desafio Tabela
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+        ?> 
