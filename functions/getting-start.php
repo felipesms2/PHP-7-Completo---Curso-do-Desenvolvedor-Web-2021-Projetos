4239a8c3594a06bab21a6aad2473a3edcd5e94ff
Felipe <felipesms@live.com>
2021-11-19 01:32:33 -0300

    Section 119 - Starting functions scope

---
+++ functions/getting-start.php
@@ -0,0 +1,9 @@
+<div class="titulo">
+        Funções Escopo
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+        ?> 
