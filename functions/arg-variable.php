e7b1481d4ede40de97d676f9503d50296ce0e8d7
Felipe <felipesms@live.com>
2021-11-20 01:38:40 -0300

    Section 123 - Starting variables parameters

---
+++ functions/arg-variable.php
@@ -0,0 +1,9 @@
+<div class="titulo">
+        Argumentos Variáveis
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+        ?> 
