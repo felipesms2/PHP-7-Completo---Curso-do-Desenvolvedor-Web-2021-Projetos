5b7177862afe982b94a38e8fe821f43cc3fba3c2
Felipe <felipesms@live.com>
2021-11-20 02:00:33 -0300

    Section 123 - Finishing variables parameters

--- functions/arg-variable.php
+++ functions/arg-variable.php
@@ -6,4 +6,30 @@
             $display = new excercise();
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
+
+            function sumNumber(...$numbers)
+                {
+                    $result =0;
+                    foreach ($numbers as $num) 
+                    {
+                       $result += $num;
+                    }
+                    return $result;
+                }
+            
+            function getPeople(...$people)
+            {
+                $person = [];
+                foreach ($people as $personName) 
+                    {
+                        $person[] = $personName;
+                    }
+                return $person;
+            }
+
+            $display->displayAndBroke(sumNumber(2,4,2));
+            $display->dumpAndBroke(getPeople("Abrantes","Josefina","Max"));
+
+
+
         ?> 
