a38ef3b3f1920d2c10f47a9d95a9183fce02f2ad
Felipe <felipesms@live.com>
2021-11-22 03:52:54 -0300

    Section 125 - Finishing data types

--- functions/data-type.php
+++ functions/data-type.php
@@ -6,19 +6,19 @@
             $display = new excercise();
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
-            function somar(int $a, int $b )
+            function sumTypes(int $a, int $b )
             {
               return($a + $b);
             }
             
-          function somarFloat(int $a, int $b ):int
+          function sumTypesFloat(int $a, int $b ):int
             {
               return($a + $b);
             }
             
             
-            echo "Resultado =" . somar(1,2) .  "<br> \n";
-            echo "Resultado =" . somar(1.7,4.9) .  "<br> \n";
-            echo "Resultado =" . somarFloat(1.7,5.5) .  "<br> \n";
+            echo "Resultado =" . sumTypes(1,2) .  "<br> \n";
+            echo "Resultado =" . sumTypes(1.7,4.9) .  "<br> \n";
+            echo "Resultado =" . sumTypesFloat(1.7,5.5) .  "<br> \n";
           
         ?> 
