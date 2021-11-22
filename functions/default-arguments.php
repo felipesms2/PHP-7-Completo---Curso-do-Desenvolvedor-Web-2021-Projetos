d9a2ff51c7ce568a54afd3e1db5c79ae84b7087d
Felipe <felipesms@live.com>
2021-11-22 03:33:52 -0300

    Section 124 - Starting default args

---
+++ functions/default-arguments.php
@@ -0,0 +1,25 @@
+<div class="titulo">
+        Argumento Padrão
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->xdisplayAndBroke($status);
+            //$display->dumpAndBroke($status);
+            function orderFood($food, $drink = "Water")
+            {
+              return "Requested food is $food and drink is $drink";
+            }
+            echo orderFood("Bife") . "<br> \n";
+            echo orderFood("Bife", "wine") . "<br> \n";
+            
+        function helloCustomer($personName="Undefined")
+          {
+             return "Hello $personName <br> \n";
+          }
+      
+            echo helloCustomer();
+            echo helloCustomer("Felipe");
+      
+        
+        ?> 
