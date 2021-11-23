23d3749a3b776c445369fd9276aad26600a99005
Felipe <felipesms@live.com>
2021-11-23 01:04:03 -0300

    Section 126 - Finishing anon functions

---
+++ functions/anon-functions.php
@@ -0,0 +1,19 @@
+<div class="titulo">
+        Funçoes Anonimas
+    </div>
+        <?php
+
+use JetBrains\PhpStorm\Internal\ReturnTypeContract;
+
+include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+
+            $anonFunc = function ()
+            {
+                return 2*2;
+            };
+
+            echo $anonFunc();
+        ?> 
