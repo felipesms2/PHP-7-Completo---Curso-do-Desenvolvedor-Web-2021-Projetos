4f671d2fb91c2501ce687dfd28d9500b393dbf13
Felipe <felipesms@live.com>
2021-11-23 05:00:13 -0300

    Section 127 - Finishing closure and callables

---
+++ functions/closure-callable.php
@@ -0,0 +1,27 @@
+<div class="titulo">
+        Closure and callable
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+
+            $soma1 = function ($a, $b) 
+            {
+                return $a + $b;
+            };
+            
+            function soma2($a, $b) {
+                return $a + $b;
+            }
+            
+            echo $soma1(2, 3) . ' ';
+            echo (is_callable($soma1) ? 'Sim' : 'Não') . '<br>';
+            
+            echo soma2(2, 3) . ' ';
+
+            //$display->dumpAndBroke(is_callable(soma2)); 
+ 
+
+        ?> 
