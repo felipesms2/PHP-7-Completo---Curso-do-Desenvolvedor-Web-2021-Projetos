226bb609724b7dc96bec468485e9918eee542e67
Felipe <felipesms@live.com>
2021-12-09 04:46:41 -0300

    Section 140 - Finishing first object and class

--- objects/fisrt-class.php
+++ objects/fisrt-class.php
@@ -6,5 +6,20 @@
             $display = new excercise();
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
-            
-        ?> 
+
+            class Customer
+                {
+                    //Atributes
+                    public $name = 'Anonimo';
+                    public $idade = 18;
+                    public function present()
+                    {
+                        echo $this->name;
+                    }
+                }
+
+            $c1 = new Customer;
+            $c1->name = "Another person";
+            $c1->present();
+            $display->displayAndBroke("");
+            $display->displayAndBroke($c1->name);
