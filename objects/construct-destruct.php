e0b610c898ec054796aa0c28194d59a38f62d52c
Felipe <felipesms@live.com>
2021-12-09 07:01:02 -0300

    Section 143 - Starting Constructor and destructor

---
+++ objects/construct-destruct.php
@@ -0,0 +1,19 @@
+<div class="titulo">
+        Construtor e Destrutor
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+            
+        class Person
+        {
+            public $name;
+            public $age;
+            public function __construct($newName, $newAge)
+            {
+
+            }
+        }
+        
