08d06dd0a19bc2ab6e742e7e8b607dd0d5696d2e
felipesms2 <felipesms@live.com>
2021-12-11 06:50:19 -0300

    Section 150 - Finishing Acces modifier

--- objects/access-modifiers.php
+++ objects/access-modifiers.php
@@ -9,9 +9,37 @@
             
         class A 
         {
-            public $public = "Públic";
-            public $protected =  "Protected";
+            public $public = "Public";
+            public $protected =  "protected";
             public $private = "Private";
-            
+            public $display;
+            public function showA($display)
+              {
+               $display->displayAndBroke($this->public);
+               $display->displayAndBroke($this->protected);
+               $display->displayAndBroke($this->private);
+              }
+           private function doNotShow($display)
+             {
+              $display->displayAndBroke("I won't show");
+             }
+           
+           protected function streamByInheritance()
+           {
+            echo "<br>Will be showed by inheritance<br>";
+           }
         }
+        $classA = new A();
+        $classA->showA($display);
+       // $classA->doNotShow($display);
         
+        class B extends A
+         {
+          public function showB()
+          {
+           parent::streamByInheritance();
+          }
+         }
+         
+         $classB = new B();
+         $classB->showB();
