71fcf73bb3d134ec7185a5acdfa704716ea863ba
Felipe <felipesms@live.com>
2021-12-11 03:11:43 -0300

    Section 150 - Starting Access Modifiers

---
+++ objects/access-modifiers.php
@@ -0,0 +1,10 @@
+<div class="titulo">
+        Modificadores de Acesso e Visibilidade
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+            
+        ?> 
