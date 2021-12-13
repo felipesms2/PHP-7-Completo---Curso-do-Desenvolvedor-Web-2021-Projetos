ddbd331441847a0fc9f2c8d680df797872fa398f
Felipe <felipesms@live.com>
2021-12-13 06:47:08 -0300

    Section 152 - Finishing Interface

--- objects/interface.php
+++ objects/interface.php
@@ -6,5 +6,20 @@
             $display = new excercise();
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
+
+            interface Animal
+                {
+                    function breath();
+                }
+            class Dog implements Animal
+            {
+                 function breath()
+                {
+                    return "Using oxigen";
+                }
+            }
+            
+        $dog1 = new Dog;
+        $display->displayAndBroke($dog1->breath());
             
         ?> 
