5f52e327296554f379c64dc0bd58436bc49c7597
Felipe <felipesms@live.com>
2021-12-12 22:06:40 -0300

    Section 152 - Starting Interface

---
+++ objects/interface.php
@@ -0,0 +1,10 @@
+<div class="titulo">
+        Interface
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+            
+        ?> 
