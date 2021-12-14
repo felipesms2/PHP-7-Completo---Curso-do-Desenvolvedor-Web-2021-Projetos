d5d8cffb9b5e724b8fe99f60c7631988d9ce89ad
felipesms2 <felipesms@live.com>
2021-12-14 09:54:50 -0300

    Section 154 - Finishing abstract class

--- objects/abstract.php
+++ objects/abstract.php
@@ -7,4 +7,44 @@
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
             
-        ?> 
+        abstract class AbsClass
+            {
+                abstract public function method1();
+                abstract public function method2($args);
+            }
+        abstract class AbsChild extends AbsClass
+            {
+                public function method1()
+                {
+                    return "Executing first method";
+                }
+
+                abstract public function thirdMethod();
+            }
+        class Concrect extends AbsChild
+            {
+                public function method1()
+                    {
+                        return "Executing first method for the second time";
+                        parent::method1();
+                    }
+                public function method2($args)
+                    {
+                        return "Method2 with args " . $args;
+                    }
+
+                public function thirdMethod()
+                    {
+                        return "thirdMethod";
+                    }
+            }
+
+        $c = new Concrect;
+        $display->displayAndBroke($c->method1());
+        $display->displayAndBroke($c->method2("abdc"));
+        $display->displayAndBroke($c->thirdMethod());
+        
+        var_dump($c instanceof Concrect);
+        var_dump($c instanceof AbsChild);
+        var_dump($c instanceof AbsClass);
+        
