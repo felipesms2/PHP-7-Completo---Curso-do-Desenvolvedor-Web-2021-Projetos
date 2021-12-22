eee3347d27adbb5d1f0f5313bb7c5cbb207cd34c
felipesms2 <felipesms@live.com>
2021-12-22 08:58:01 -0300

    Section 157 - Starting final modifiers

---
+++ objects/final-modifiers.php
@@ -0,0 +1,10 @@
+<div class="titulo">
+        Modificadores Finais
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+            
+        ?> 
