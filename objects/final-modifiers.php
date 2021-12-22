a24945604dbd314ddffdf7264228932b9a2e3a65
felipesms2 <felipesms@live.com>
2021-12-22 09:16:41 -0300

    Section 157 - Finishing final modifiers

--- objects/final-modifiers.php
+++ objects/final-modifiers.php
@@ -6,5 +6,44 @@
             $display = new excercise();
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
-            
+
+
+        abstract class SomeNameABS
+            {
+                abstract public function method1();
+               final function method2()
+                {
+                    echo "I won't change! <br>";
+                }
+
+            }
+
+        class ChildSome extends SomeNameABS
+        {
+             public function method1()
+            {
+                echo "Executing method 1 <br>";
+            }
+
+
+        }
+
+        final class NoInheritance 
+        {
+            public $msg = "Props and methods on this class cannot be inhherited";
+            public function generalAlert()
+            {
+                return $this->msg;
+            }
+        }
+        
+        
+        $testing = new ChildSome();
+        $testing->method1();
+        $testing->method2();
+
+        $noInheritance = new NoInheritance();
+
+        $display->displayAndBroke($noInheritance->generalAlert());
+
         ?> 
