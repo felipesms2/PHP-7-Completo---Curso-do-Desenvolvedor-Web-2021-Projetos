73da5b606392c3abc0f97448080dd193ab232f0d
felipesms2 <felipesms@live.com>
2021-12-23 06:08:17 -0300

    Section 159 - Keeping traits 02

--- objects/traits-02.php
+++ objects/traits-02.php
@@ -2,9 +2,34 @@
         Traits 02
     </div>
         <?php
-            include("customClass.php");
+
+use betterValidation as GlobalBetterValidation;
+
+include("customClass.php");
             $display = new excercise();
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
-            
-        ?> 
+
+        trait strValidation 
+            {
+                public function validateString($str)
+                {
+                    return isset($str) && $str !=="";
+                }
+            }
+        trait betterValidation 
+            {
+                public function validateString($str)
+                {
+                    return isset($str) && $str !=="";
+                }
+            }
+        class UserRoles
+        {
+            use strValidation, betterValidation
+                {
+                    betterValidation::validateString insteadOf strValidation;
+                }
+        }
+        
+        
