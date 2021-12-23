4a5438f76e215828f1d75dd98dc324338df380f7
felipesms2 <felipesms@live.com>
2021-12-23 00:56:13 -0300

    Section 158 - Finishing traits 01

--- objects/traits-01.php
+++ objects/traits-01.php
@@ -2,9 +2,35 @@
         Traits 01
     </div>
         <?php
-            include("customClass.php");
+
+use User as GlobalUser;
+
+include("customClass.php");
             $display = new excercise();
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
             
-        ?> 
+        trait validation        
+            {
+                public function stringValidation($str)
+                {
+                    return isset($str) && $str !== "";
+                }
+            }
+        trait betterValidation        
+            {
+                public function betterStrValidation($str)
+                {
+                    return isset($str) && trim($str);
+                }
+            }
+
+        class UserTrait 
+        {
+            use validation, betterValidation;
+        }
+        
+        $user = new UserTrait();
+
+        $display->dumpAndBroke($user->stringValidation(" "));
+        $display->dumpAndBroke($user->betterStrValidation(" "));
