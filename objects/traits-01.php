46302a34cdf2a40be861d8bb13572f7718e31c16
felipesms2 <felipesms@live.com>
2021-12-23 00:34:48 -0300

    Section 158 - Starting traits 01

---
+++ objects/traits-01.php
@@ -0,0 +1,10 @@
+<div class="titulo">
+        Traits 01
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+            
+        ?> 
