fb525795d2f9498feca9e8e7f70b3797a5665f77
felipesms2 <felipesms@live.com>
2022-01-07 04:19:41 -0300

    Section 169 - Finishing require and return

--- includes/require-return.php
+++ includes/require-return.php
@@ -6,5 +6,12 @@
             $display = new excercise();
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
+
+            $returnedValue = require("return-used.php");
+            $returnedValue2 = require(__DIR__ . "/return-not-used.php");
+            $display->displayAndBroke($returnedValue);
+            $display->displayAndBroke(__DIR__);
+            $display->displayAndBroke($returnedValue2);
+            $display->displayAndBroke($justDeclared);
             
         ?> 
