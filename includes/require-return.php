890740bed85f3b0c1cc6111a3b4874fbc5eb6f79
felipesms2 <felipesms@live.com>
2022-01-07 04:12:05 -0300

    Section 169 - Staring require and return

---
+++ includes/require-return.php
@@ -0,0 +1,10 @@
+<div class="titulo">
+        Require && Return
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+            
+        ?> 
