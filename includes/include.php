ac0270ac84aacde2dfe6870097f5a51bd2760c4e
felipesms2 <felipesms@live.com>
2022-01-07 00:47:06 -0300

    Section 166 - Finishing include

--- includes/include.php
+++ includes/include.php
@@ -1,3 +1,12 @@
+<div class="titulo">
+Include
+</div>
 <?php
+    include("customClass.php");
+    $display = new excercise();
+    //$display->displayAndBroke($status);
+    //$display->dumpAndBroke($status);
+    include("include_file.php");
+    $display->dupmAndPre($varInclude);
+?> 
 
-include("include_file.php");
