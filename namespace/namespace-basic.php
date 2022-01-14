db62edb109c1f0742abdfa78a7e7de6165be20ec
felipesms2 <felipesms@live.com>
2022-01-14 03:07:39 -0300

    Section 177 - Finishing namespace

--- namespace/namespace-basic.php
+++ namespace/namespace-basic.php
@@ -1,10 +1,18 @@
+<?php namespace contextName ; ?>
 <div class="titulo">
         Namespace Básico
     </div>
         <?php
             include("customClass.php");
-            $display = new excercise();
-            //$display->displayAndBroke($status);
-            //$display->dumpAndBroke($status);
+            $display = new \excercise();
+            $display->displayAndBroke(__NAMESPACE__);
+            const constant1 = 123;
+            define("contextName\constant2", 234);
+            define( __NAMESPACE__ . "\constant3" , 345);
+            define("\otherContext\constant1" , "OtherContext");
+            $display->dumpAndBroke(constant1);
+            $display->dumpAndBroke(constant2);
+            $display->dumpAndBroke(\contextName\constant3);
+            $display->dumpAndBroke(constant3);
             
         ?> 
