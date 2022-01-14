1fd9c2f6fbc6d3355b3f5266561acd06ba5ea4b3
felipesms2 <felipesms@live.com>
2022-01-14 03:55:29 -0300

    Section 178 - Starting namespace

---
+++ namespace/sub-namespace.php
@@ -0,0 +1,10 @@
+<div class="titulo">
+        Sub Namespace
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+            
+        ?> 
