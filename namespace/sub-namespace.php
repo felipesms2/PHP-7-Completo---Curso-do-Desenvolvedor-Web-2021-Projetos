9b7ad14980c25e83edda661f903610f9a8345433
felipesms2 <felipesms@live.com>
2022-01-15 14:51:41 -0300

    Section 178 finishing Sub Namespace

--- namespace/sub-namespace.php
+++ namespace/sub-namespace.php
@@ -1,10 +1,16 @@
+<?php namespace App; ?>
 <div class="titulo">
         Sub Namespace
     </div>
         <?php
             include("customClass.php");
-            $display = new excercise();
+            $display = new \excercise();
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
-            
+
+            $display->displayAndBroke(__NAMESPACE__);
+            namespace App\Main;
+            $display->displayAndBroke(__NAMESPACE__);
+            namespace App\Main\Specifc;
+            $display->displayAndBroke(__NAMESPACE__);
         ?> 
