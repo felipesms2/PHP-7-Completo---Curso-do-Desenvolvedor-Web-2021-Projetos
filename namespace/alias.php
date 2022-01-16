e812383f974c6c9eddde013651225edd763f2da3
felipesms2 <felipesms@live.com>
2022-01-16 01:27:23 -0300

    Section 179 Finishing constant chapter

---
+++ namespace/alias.php
@@ -0,0 +1,23 @@
+<?php namespace Name\Very\Big;
+            $display->displayAndBroke(__NAMESPACE__);
+            const SOMECONST = 123;
+            const OTHER = "BIGNAME";
+            function personalSum($a, $b)
+            {
+                return $a  +$b;
+            }
+
+        class PersonalClass extends \excercise
+            {
+                public $var;
+                public function func()
+                    {
+                        echo __NAMESPACE__ . " -> " .
+                        __CLASS__ . " -> " . __METHOD__ . "<br>"
+                        ;
+                    }
+            }
+        
+
+
+
--- namespace/use-alias.php
+++ namespace/use-alias.php
@@ -1,11 +1,25 @@
-<div class="titulo">
+<?php namespace Other\Name; ?>
+
+    <div class="titulo">
         Use Alias
     </div>
-        <?php
-            include("customClass.php");
-            $display = new excercise();
-            //$display->displayAndBroke($status);
-            //$display->dumpAndBroke($status);
+
+<?php
+
+    const OTHER = "other";
+    include("customClass.php");
+    $display = new \excercise();
+    $display->displayAndBroke(OTHER);
+    echo __NAMESPACE__ . "<br>";
+    include("./namespace/alias.php");
+
+    use \Name\Very\Big as NVB;
+
+    echo NVB\OTHER;
+    
+
+
+    ?>
+
             
-        ?> 
-        
+
