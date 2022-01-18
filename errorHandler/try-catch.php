4a151e4be55987018e2608b1e8cba84c41b99063
felipesms2 <felipesms@live.com>
2022-01-18 16:05:09 -0300

    Section 184 - Starting Try Catch

---
+++ errorHandler/try-catch.php
@@ -0,0 +1,10 @@
+<div class="titulo">
+        Try Catch
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+            
+        ?> 
