e944f768449b6c6cfbb3661708e34913379ff229
felipesms2 <felipesms@live.com>
2022-01-22 10:11:19 -0300

    Section 184 - Finishing basic try catch

--- errorHandler/try-catch.php
+++ errorHandler/try-catch.php
@@ -6,5 +6,32 @@
             $display = new excercise();
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
+
+
+
             
-        ?> 
+        try
+            {
+                echo intdiv(7,0);
+            }
+        catch(Error $e)
+            {
+                $display->displayAndBroke("An error has been ocurred");
+            }
+        try
+            {
+                throw new Exception("Another stranger error");
+                echo intdiv(7,0);
+            }
+        catch(DivisionByZeroError $e)
+            {
+                $display->displayAndBroke("Division by zero");
+            }
+        catch(Throwable $e)
+            {
+                $display->displayAndBroke("Error has been found " . $e->getMessage());
+            }
+        finally{
+            $display->displayAndBroke("Finally executing a statment");
+        }
+        
