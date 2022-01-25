bc425e5720654ec3b3a52bca4122986a5ed8260a
felipesms2 <felipesms@live.com>
2022-01-25 20:21:44 -0300

    Section 185 - Finishing custom errors

---
+++ errorHandler/custom_errors.php
@@ -0,0 +1,52 @@
+<div class="titulo">
+        Custom Error
+    </div>
+        <?php
+            include("customClass.php");
+            $display = new excercise();
+            //$display->displayAndBroke($status);
+            //$display->dumpAndBroke($status);
+
+     
+
+     
+            // echo 7 / 0;
+            class AgeRetireException extends Exception {
+                public function __construct($message, $code = 0,
+                    $previous = null) {
+                    parent::__construct($message, $code, $previous);
+                }
+            }
+            
+            function retireTimeCalculate($age) {
+                if($age < 18) 
+                {
+                    throw new AgeRetireException(
+                        'Too young');
+                }
+            
+                if($age > 70) 
+                {
+                    throw new AgeRetireException(
+                        'Must have retired');
+                }
+                
+                return 70 - $age;
+            }
+            
+            $agesToEvaluate = [15, 30, 60, 80, "asdf"];
+            
+            foreach($agesToEvaluate as $age) 
+            {
+                try {
+                        $remainingTime = retireTimeCalculate($age);
+                        echo "Age: $age, $remainingTime years left<br>";
+                    } 
+                catch(AgeRetireException $e) 
+                {
+                    echo "Impossible to calculate $age years.<br>";
+                    echo "Reason: {$e->getMessage()}<br>";
+                }
+            }
+            
+            echo "Fim!";
