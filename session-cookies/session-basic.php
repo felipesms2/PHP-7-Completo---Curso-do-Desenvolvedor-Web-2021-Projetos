79af9a0e81d407838b32e54fdb9d886cba1542e7
felipesms2 <felipesms@live.com>
2022-01-28 12:47:11 -0300

    Section 193 - Finishing basic session

---
+++ session-cookies/session-basic.php
@@ -0,0 +1,24 @@
+<div class="titulo">
+        Session
+    </div>
+        <?php
+        session_start();
+        include("customClass.php");
+        $display = new excercise();
+        //$display->displayAndBroke($status);
+        //$display->dumpAndBroke($status);
+        $display->dumpAndBroke($_SESSION);
+
+        if (!isset($_SESSION['name'])) 
+            {
+                $_SESSION['name'] = "Felipe";
+            }
+        $display->dumpAndBroke($_SESSION);
+        //session_destroy();
+
+        ?>
+
+    <a href="somefile.php">
+        Change Session
+    </a>
+        
