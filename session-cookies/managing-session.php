5db59b1ef7e9bdaac23ef142beabf9e8c8f325d5
felipesms2 <felipesms@live.com>
2022-01-29 15:49:25 -0300

    Section 196 - Finishing managing session

--- session-cookies/managing-session.php
+++ session-cookies/managing-session.php
@@ -2,7 +2,21 @@
         Managing Session
     </div>
         <?php
+            //session_id("coui66dnhlokjbq5j5qsk83c09");
+            session_start();
+            echo session_id();
+            $counter = &$_SESSION['counter'];
+            $counter = $counter ?  $counter+1 : 1; 
+
+            if ($counter %5 ===0) 
+                {
+                    session_regenerate_id();
+                }
+
             include("customClass.php");
             $display = new excercise();
+            $display->displayAndBroke("<br>");
+            $display->displayAndBroke($counter);
             //$display->displayAndBroke($status);
             //$display->dumpAndBroke($status);
+
