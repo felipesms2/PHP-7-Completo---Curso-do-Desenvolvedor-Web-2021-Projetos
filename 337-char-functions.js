2765a309bf5eaba7e52265eb6c3ea418d5088ba5
felipesms <felipesms@live.com>
2022-06-29 11:42:19 -0300

    Section 337 - Finishing sring operations

---
+++ 337-char-functions.js
@@ -0,0 +1,7 @@
+    
+    const e = "Cod3R";
+    // console.log(e.charAt(0));
+    // console.log(e.charCodeAt(3));
+    // console.log(e.substring(2));
+    // console.log(e.substring(1,4));
+    console.log("School ".concat(e).concat(" !"));
