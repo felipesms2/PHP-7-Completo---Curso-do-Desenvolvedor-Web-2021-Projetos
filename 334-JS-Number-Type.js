d061324cd105e0f961f29b77b4b70cb1f2e08337
felipesms <felipesms@live.com>
2022-06-29 09:57:53 -0300

    Section 334 - Finishing additional info about type number

---
+++ 334-JS-Number-Type.js
@@ -0,0 +1,9 @@
+
+console.clear();
+let media = 8.5646546588;
+console.log(media);
+console.log(media.toString(2));
+console.log(media.toFixed(2)); // Convert to binary
+console.log(media.toFixed());
+console.log(typeof media);
+
