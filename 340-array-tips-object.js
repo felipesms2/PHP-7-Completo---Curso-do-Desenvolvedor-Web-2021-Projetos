fbab8d34ac119dfa0af95d15292b34974b2aae10
felipesms <felipesms@live.com>
2022-07-01 13:55:51 -0300

    Section 340 finishing types array

---
+++ 340-array-tips-object.js
@@ -0,0 +1,8 @@
+const val = [10, 20, 30.5, 40, "a"];
+console.clear();
+val.push({id:3}, "abc")
+console.log(val);
+console.log(val.length);
+console.log(val.pop());
+console.log(val.length);
+console.log(typeof val);
