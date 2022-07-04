cc4e8429626411349e4be565f5f1dfd443bbc852
felipesms <felipesms@live.com>
2022-07-03 23:50:17 -0300

    section_357_finishing

---
+++ 357-operadores-destructing.js
@@ -0,0 +1,17 @@
+const people = 
+    {
+        Pname: "random",
+        age: 10,
+        city: 'NY',
+        Paddress: 
+            {
+                street: "x",
+                num: 20
+            }
+    }
+const {Pname, age} = people
+console.log(Pname, age);
+
+const {Paddress: {street}} = people
+
+console.log(street);
