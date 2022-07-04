70d5c3a9347fefecb2d2ab19fa3ec4b6fffe6218
felipesms <felipesms@live.com>
2022-07-03 21:41:58 -0300

    Section 345 - done

---
+++ 345-basic-function-sample.js
@@ -0,0 +1,22 @@
+// Sample Error NaN (not a number)
+
+function sumNumber(a,b)
+    {
+        return a+b;
+    }
+console.log(sumNumber(1));
+
+// Arrow Function
+
+const sumAnother = (a,b) =>
+    {
+        return a + b
+    }
+
+console.log(sumAnother(3,4));
+
+// Removing return
+
+const dropOff = (a, b) => a-b;
+console.log(dropOff(2,5));
+
